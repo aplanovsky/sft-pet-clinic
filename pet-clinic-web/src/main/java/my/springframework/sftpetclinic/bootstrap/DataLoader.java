742d6f27fbd25a37dba47e5fe6e4b09a0e140784
aplanovsky <aplanovsky@gmail.com>
2019-07-19 14:54:41 +0300

    Added Pets and Contact info Owners on startup. Closes #26

--- pet-clinic-web/src/main/java/my/springframework/sftpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/my/springframework/sftpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package my.springframework.sftpetclinic.bootstrap;
 
 import my.springframework.sftpetclinic.model.Owner;
+import my.springframework.sftpetclinic.model.Pet;
 import my.springframework.sftpetclinic.model.PetType;
 import my.springframework.sftpetclinic.model.Vet;
 import my.springframework.sftpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import my.springframework.sftpetclinic.services.PetTypeService;
 import my.springframework.sftpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -41,12 +44,31 @@ public class DataLoader implements CommandLineRunner {
     Owner owner1 = new Owner();
     owner1.setFirstName("Michael");
     owner1.setLastName("Weston");
+    owner1.setAddress("SomeHome SomeStreet");
+    owner1.setCity("SomeCity");
+    owner1.setTelephone("SomePhone");
 
+    Pet makesPet = new Pet();
+    makesPet.setPetType(sevedDogPetType);
+    makesPet.setOwner(owner1);
+    makesPet.setBirthDate(LocalDate.now());
+    makesPet.setName("Rosco");
+    owner1.getPets().add(makesPet);
     ownerService.save(owner1);
 
     Owner owner2 = new Owner();
     owner2.setFirstName("Fiona");
     owner2.setLastName("Glenanne");
+    owner2.setAddress("SomeHome SomeStreet");
+    owner2.setCity("SomeCity");
+    owner2.setTelephone("SomePhone");
+
+    Pet fionasCat = new Pet();
+    fionasCat.setName("Just Cat");
+    fionasCat.setOwner(owner2);
+    fionasCat.setBirthDate(LocalDate.now());
+    fionasCat.setPetType(sevedCatPetType);
+    owner2.getPets().add(fionasCat);
 
     ownerService.save(owner2);
 
