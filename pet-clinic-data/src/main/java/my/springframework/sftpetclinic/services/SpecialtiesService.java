fecb02718bac03bf1541b4f5a9e49689544c334b
aplanovsky <aplanovsky@gmail.com>
2019-07-19 15:17:16 +0300

    added Specialty Map Service impl. Closes #24

---
+++ pet-clinic-data/src/main/java/my/springframework/sftpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package my.springframework.sftpetclinic.services;
+
+import my.springframework.sftpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
