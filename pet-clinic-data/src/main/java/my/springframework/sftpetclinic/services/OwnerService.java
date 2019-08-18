4ecfae4d08b4e54e926a499304d05fd4cdebe2fe
aplanovsky <aplanovsky@gmail.com>
2019-08-19 02:12:36 +0300

    impl find owner. Closes #46

--- pet-clinic-data/src/main/java/my/springframework/sftpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/my/springframework/sftpetclinic/services/OwnerService.java
@@ -2,10 +2,12 @@ package my.springframework.sftpetclinic.services;
 
 import my.springframework.sftpetclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
   Owner findByLastName(String lastName);
 
-
+  List<Owner> findAllByLastNameLike(String lastName);
 }
