25e1c5554cd33caea9859943b2e305c312ade5ca
aplanovsky <aplanovsky@gmail.com>
2019-08-21 02:35:13 +0300

    Added add and update pet. Closes #48

--- pet-clinic-data/src/main/java/my/springframework/sftpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/my/springframework/sftpetclinic/model/Owner.java
@@ -11,14 +11,12 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
-
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
-  @Builder
-  public Owner(Long id, String firstName, String lastName,
-               String address, String city, String telephone, Set<Pet> pets) {
 
+  @Builder
+  public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
     super(id, firstName, lastName);
     this.address = address;
     this.city = city;
@@ -38,5 +36,24 @@ public class Owner extends Person {
   @ManyToMany(cascade = CascadeType.ALL, mappedBy = "owner")
   private Set<Pet> pets = new HashSet<>();
 
+  /**
+   * Return the Pet with the given name, or null if none found for this Owner.
+   *
+   * @param name to test
+   * @return true if pet name is already in use
+   */
 
+  public Pet getPet(String name, boolean ignoreNew){
+    name = name.toLowerCase();
+    for (Pet pet : pets){
+      if (!ignoreNew || !pet.isNew()){
+        String compName = pet.getName();
+        compName = compName.toLowerCase();
+        if (compName.equals(name)){
+          return pet;
+        }
+      }
+    }
+    return null;
+  }
 }
